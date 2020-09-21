fe662c5acede8648b9697606789489800e067c3e
Ian Leefmans <iel230@nyu.edu>
2020-09-21 15:51:14 -0400

    Create setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(
+    name="Re-Identifying_Persistent_Skin_Conditions",
+    author="Ian Leefmans",
+    author_email="ian.e.leefmans@gmail.com",
+    url="https://https://github.com/ileefmans/Re-Identifying_Persistent_Skin_Conditions",
+    packages=["skinConditionDetect"],
+)
