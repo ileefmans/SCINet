0bd6708b8c78f06382ea70c8f1877ba038f51d5b
Ian Leefmans <iel230@nyu.edu>
2020-09-21 23:02:46 -0400

    Fix typos

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,6 +1,7 @@
 import unittest
 #from skinConditionDetect.preprocess import image_process
-from preprocess import image_process
+#from preprocess import image_process
+import skinConditionDetect
 import torch
 from PIL import Image
 import requests
