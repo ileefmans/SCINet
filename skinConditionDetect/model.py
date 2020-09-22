18118745a3da3a9fc29e66359233d9575c332d40
Ian Leefmans <iel230@nyu.edu>
2020-09-22 10:07:47 -0400

    Create MVP - pretrained Faster-RCNN

--- skinConditionDetect/model.py
+++ skinConditionDetect/model.py
@@ -1,14 +1,22 @@
 import torch
 import torch.nn as nn
 import torch.optim 
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
 
 
-class STN(nn.Module):
+class MVP(nn.Module):
 	""" 
 		Class for Spatial Transformer Network (STN)
 	"""
 
 	def __init__(self):
-		super(STN, self).__init__()
+		super(MVP, self).__init__()
 
-		pass
+		self.model = fasterrcnn_resnet50_fpn(pretrained=True, progress=True, num_classes=7)
+
+
+	def forward(self, x):
+		x = self.model(x)
+
+
+		return(x)
