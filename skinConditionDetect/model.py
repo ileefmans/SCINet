691ba18b7789e60d6d077b423dfd3bd0ed4a482c
Ian Leefmans <iel230@nyu.edu>
2020-09-21 21:27:47 -0400

    Add model.py

---
+++ skinConditionDetect/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim 
+
+
+class STN(nn.Module):
+	""" 
+		Class for Spatial Transformer Network (STN)
+	"""
+
+	def __init__(self):
+		super(STN, self).__init__()
+
+		pass
