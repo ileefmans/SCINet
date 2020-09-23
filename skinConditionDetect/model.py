f91b9589689f8683822713bddf394e0c0af8e145
Ian Leefmans <iel230@nyu.edu>
2020-09-23 10:29:49 -0400

    Replace classification head of pretrained model

--- skinConditionDetect/model.py
+++ skinConditionDetect/model.py
@@ -6,13 +6,16 @@ from torchvision.models.detection import fasterrcnn_resnet50_fpn
 
 class MVP(nn.Module):
 	""" 
-		Class for Spatial Transformer Network (STN)
+		Class for Pretrained Faster-RCNN
 	"""
 
 	def __init__(self):
 		super(MVP, self).__init__()
 
-		self.model = fasterrcnn_resnet50_fpn(pretrained=False, progress=True, num_classes=7)
+		self.model = fasterrcnn_resnet50_fpn(pretrained=True)
+		self.num_classes = 7
+		self.in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+		self.model.roi_heads.box_predictor = FastRCNNPredictor(self.in_features, self.num_classes)
 
 
 	def forward(self, image, annotations):
