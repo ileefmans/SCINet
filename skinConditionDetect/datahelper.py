074b537cb6a746615129793581276a16e3cdec99
Ian Leefmans <iel230@nyu.edu>
2020-09-25 00:46:08 -0400

    Reduce subset of data trained

--- skinConditionDetect/datahelper.py
+++ skinConditionDetect/datahelper.py
@@ -162,7 +162,7 @@ class CreateDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         #return len(self.annotation_dict)
-        return 1000
+        return 500
 
     def __getitem__(self, index):
 
