e1931b9ae8b3f76ac2d156d40b1daa1bb85eace6
Ian Leefmans <iel230@nyu.edu>
2020-09-26 12:43:43 -0400

    Expand dataloader to include enire test set

--- skinConditionDetect/datahelper.py
+++ skinConditionDetect/datahelper.py
@@ -161,8 +161,8 @@ class CreateDataset(torch.utils.data.Dataset):
     
 
     def __len__(self):
-        #return len(self.annotation_dict)
-        return 500
+        return len(self.annotation_dict)
+        #return 500
 
     def __getitem__(self, index):
 
