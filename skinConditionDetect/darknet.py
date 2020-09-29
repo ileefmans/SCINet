7bc5889a683254aa6a89d1dc5e55d065f2747e2e
Ian Leefmans <iel230@nyu.edu>
2020-09-28 21:25:09 -0400

    Finish darknet.py - add empty layer and detection layer

--- skinConditionDetect/darknet.py
+++ skinConditionDetect/darknet.py
@@ -129,11 +129,57 @@ def create_modules(blocks):
 			else:
 				filters = output_filters[index+start]
 
-			# Define skip connection
+		# Define skip connection
 		elif (x['type'] == 'shortcut'):
 			shortcut = EmptyLayer()
 			module.add_module("shortcut_{}".format(index), shortcut)
-			
+
+		# Define Detection Layer
+		elif (x['type'] == 'yolo'):
+			mask = x['mask'].split(',')
+			mask = [int(x) for x in mask]
+
+			anchors = x['anchors'].split(',')
+			anchors = [int(a) for a in anchors]
+			anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors), 2)]
+			anchors = [anchors[i] for i in mask]
+
+			detection = DetectionLayer(anchors)
+			module.add_module("Detection_{}".format(index), detection)
+
+		module_list.append(module)
+		prev_filters = filters
+		output_filters.append(filters)
+
+	return (net_info, module_list)
+
+
+
+
+
+
+
+
+
+
+
+#Create Empty Layer module
+class EmptyLayer(nn.Module):
+	"""
+		Class for Empty Layer
+	"""
+	def __init__(self):
+		super(EmptyLayer, self).__init__()
+
+#Create Detection Layer module
+class DetectionLayer(nn.Module):
+	"""
+		Class for Detection Layer
+	"""
+	def __init__(self, anchors):
+		super(DetectionLayer, self).__init__()
+		self.anchors = anchors
+
 
 
 
