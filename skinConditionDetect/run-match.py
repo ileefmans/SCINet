86a7c302f37cdfbac49149c2954f2edbe0b0c83f
Ian Leefmans <iel230@nyu.edu>
2020-09-30 22:49:56 -0400

    Add argument parser

--- skinConditionDetect/run-match.py
+++ skinConditionDetect/run-match.py
@@ -4,8 +4,14 @@ from facealign import FaceAlign, CalculateMatches
 def get_args():
     parser = argparse.ArgumentParser(description = "Model Options")
     parser.add_argument("-p", "--shape_predictor", required=True, help="path to facial landmark predictor")
-    parser.add_argument("-i", "--image1", required=True, help="path to first input image")   
-    parser.add_argument("-i", "--image1", required=True, help="path to second input image")  
+    parser.add_argument("-i", "--sample1", required=True, help="path to first input image")   
+    parser.add_argument("-i", "--sample1", required=True, help="path to second input image")  
+    parser.add_argument("--local", type=bool, default=False, help="False if running on AWS, True if running locally")
+    parser.add_argument("--local_pickle_path", type=str, default="/Users/ianleefmans/Desktop/Insight/Project/Re-Identifying_Persistent_Skin_Conditions/skinConditionDetect/pickle/train_annotation_dict.pkl", help="path to local pickled annotation path dictionary")
+	parser.add_argument("--remote_pickle_path", type=str, default="train_annotation_dict.pkl")
+	parser.add_argument("--local_data_directory", type=str, default="/Users/ianleefmans/Desktop/Insight/Project/Data", help="Path to data")
+	parser.add_argument("--remote_data_directory", type=str, default="<blank>", help="no remote data dictionary applicable")
+	parser.add_argument("--shuffle", type=bool, default=True, help="True if dataloader shuffles input samples before batching, False if samples are batched in order")
     return vars(parser.parse_args())
 
 
@@ -14,3 +20,6 @@ def get_args():
 
 class GeoMatch:
 	def __init__(self):
+
+		self.ops = get_args()
+		self.im
