5c0d8c45b11c68c040e12bb4bc1c5eff2137f240
Ian Leefmans <iel230@nyu.edu>
2020-10-05 12:30:35 -0400

    Return IoU results, need to test and then include confidence

--- skinConditionDetect/test.py
+++ skinConditionDetect/test.py
@@ -82,121 +82,149 @@ class Test:
 			self.test_loader = DataLoader(dataset=self.testset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=self.shuffle, collate_fn=my_collate2)
 
 
-		def calculate_metric(self):
-			pass
+	def calculate_metric(self):
+		pass
 
-		def process_for_inference(self, box, witdth, height):
-			# Create blank image size of picture
-			box_im = Image.new('RGB', (width, height))
-			box_im = cv2.cvtColor(np.array(box1_im), cv2.COLOR_RGB2BGR)
+	def process_for_inference(self, box, witdth, height):
+		# Create blank image size of picture
+		box_im = Image.new('RGB', (width, height))
+		box_im = cv2.cvtColor(np.array(box1_im), cv2.COLOR_RGB2BGR)
 
-			# Get dimensions for bboxes
-			x1 = int(box[0])
-			y1 = int(box[1])
-			x2 = int(box[2])
-			y2 = int(box[3])
+		# Get dimensions for bboxes
+		x1 = int(box[0])
+		y1 = int(box[1])
+		x2 = int(box[2])
+		y2 = int(box[3])
 
-			# Draw bounding boxes
-			cv2.rectangle(box_im,(x1,y1),(x2,y2),(0,255,0),-1) 
+		# Draw bounding boxes
+		cv2.rectangle(box_im,(x1,y1),(x2,y2),(0,255,0),-1) 
 
-			# Transform to PIL Image for preprocessing before entering SCINet
-			box_im = cv2.cvtColor(box_im, cv2.COLOR_BGR2RBG)
-			box_im = Image.fromarray(box_im)
-			box_im = box_im.convert('RGB')
+		# Transform to PIL Image for preprocessing before entering SCINet
+		box_im = cv2.cvtColor(box_im, cv2.COLOR_BGR2RBG)
+		box_im = Image.fromarray(box_im)
+		box_im = box_im.convert('RGB')
 
-			# Preprocess
-			image_process = Image_Process((256,256))
-			box_im = image_process.expand(box_im)
-			transform = torchvision.transforms.ToTensor()
-			box_im = transform(box_im)
-			box_im = image_process.uniform_size(box_im)
+		# Preprocess
+		image_process = Image_Process((256,256))
+		box_im = image_process.expand(box_im)
+		transform = torchvision.transforms.ToTensor()
+		box_im = transform(box_im)
+		box_im = image_process.uniform_size(box_im)
 
-			return box_im
-
-
-		def IoU(self, input1, input2):
-			union = np.count_nonzero(input1 + input2)
-			total  = np.count_nonzero(input1) + np.count_nonzero(input2)
-			intersection = total-union
-			IoU = intersection/union
-
-			return IoU
+		return box_im
 
 
+	def IoU(self, input1, input2):
+		union = np.count_nonzero(input1 + input2)
+		total  = np.count_nonzero(input1) + np.count_nonzero(input2)
+		intersection = total-union
+		IoU = intersection/union
+
+		return IoU
 
 
-		def evaluate(self):
-
-			if self.local==False:
-				torch.set_default_tensor_type(torch.cuda.FloatTensor)
-				print("\n \n EVERYTHING TO CUDA \n \n")
-
-			# Load weights if applicable
-			if self.load_weights == True:
-				start_epoch, loss = self.load_checkpoint(self.model, self.optimizer, self.model_name)
-				print("\n \n [WEIGHTS LOADED]")
-
-			with torch.no_grad():
-				self.model.eval()
-
-				for image1, image2, annotation1, annotation2 in tqdm(self.val_loader, desc= "Validation Epoch "+str(epoch)):
-
-					# Get dimensions of both images
-					width1 = image1.size(-1)
-					height1 = image1.size(-2)
-					width2 = image2.size(-1)
-					height2 = image2.size(-2)
-
-					# Number of boxes for each image
-					num_boxes1 = len(annotation1[0]['boxes'].size(0))
-					num_boxes2 = len(annotation2[0]['boxes'].size(0))
 
 
-					# Blank lists where output tensors will go for each image's bounding boxes
-					output_im1 = []
-					output_im2 = []
+	def evaluate(self):
 
-					# Loop through each bounding box for image 1
-					for i in range(max(num_boxes1, num_boxes2)):
-						
-						# Get bounding boxes
-						try:
-							box1 = annotation1[0]['boxes'][i,:]
-						except:
-							# if out of range for image with less boxes, fill in blank box to feed to other branch of SCINet
-							box1 = [0, 0, 0, 0]
-						finally:
-							pass
-						try:
-							box2 = annotation2[0]['boxes'][i,:]
-						except:
-							# if out of range for image with less boxes, fill in blank box to feed to other branch of SCINet
-							box2 = [0, 0, 0, 0]
-						finally:
-							pass
+		if self.local==False:
+			torch.set_default_tensor_type(torch.cuda.FloatTensor)
+			print("\n \n EVERYTHING TO CUDA \n \n")
 
-						# process for inference with SCINet
-						box1_im = self.process_for_inference(box1, width1, height1)
-						box2_im = self.process_for_inference(box2, width2, height2)
+		# Load weights if applicable
+		if self.load_weights == True:
+			start_epoch, loss = self.load_checkpoint(self.model, self.optimizer, self.model_name)
+			print("\n \n [WEIGHTS LOADED]")
 
-						# Feed to SCINet
-						box1_im = box1_im.to(self.device)
-						box2_im = box2_im.to(self.device)
+		with torch.no_grad():
+			self.model.eval()
 
-						x1_hat, x2_hat, z1, z2 = self.model(box1_im, box2_im)
+			results = []
+			for image1, image2, annotation1, annotation2 in tqdm(self.val_loader, desc= "Validation Epoch "+str(epoch)):
 
-						# Append applicable results to list 
-						if i<num_boxes1:
-							output_im1.append(x1_hat)
-						if i<num_boxes2:
-							output_im2.append(x2_hat)
+				# Get dimensions of both images
+				width1 = image1.size(-1)
+				height1 = image1.size(-2)
+				width2 = image2.size(-1)
+				height2 = image2.size(-2)
+
+				# Number of boxes for each image
+				num_boxes1 = len(annotation1[0]['boxes'].size(0))
+				num_boxes2 = len(annotation2[0]['boxes'].size(0))
+
+
+				# Blank lists where output tensors will go for each image's bounding boxes
+				output_im1 = []
+				output_im2 = []
+
+				# Loop through each bounding box for image 1
+				for i in range(max(num_boxes1, num_boxes2)):
 					
-					matched = []
-					for i in range(len(output_im1)):
-						for j in range(len(output_im2)):
-							if annotation1[0]['labels'][i] == annotation2[0]['labels'][j]:
-								IoU = self.IoU(output_im1, output_im2)
-								
+					# Get bounding boxes
+					try:
+						box1 = annotation1[0]['boxes'][i,:]
+					except:
+						# if out of range for image with less boxes, fill in blank box to feed to other branch of SCINet
+						box1 = [0, 0, 0, 0]
+					finally:
+						pass
+					try:
+						box2 = annotation2[0]['boxes'][i,:]
+					except:
+						# if out of range for image with less boxes, fill in blank box to feed to other branch of SCINet
+						box2 = [0, 0, 0, 0]
+					finally:
+						pass
+
+					# process for inference with SCINet
+					box1_im = self.process_for_inference(box1, width1, height1)
+					box2_im = self.process_for_inference(box2, width2, height2)
+
+					# Feed to SCINet
+					box1_im = box1_im.to(self.device)
+					box2_im = box2_im.to(self.device)
+
+					x1_hat, x2_hat, z1, z2 = self.model(box1_im, box2_im)
+
+					# Append applicable results to list 
+					if i<num_boxes1:
+						output_im1.append(x1_hat)
+					if i<num_boxes2:
+						output_im2.append(x2_hat)
+				
+
+				# calculate boxes with highest IoU
+				matched_boxes = {}
+				box_list = []
+				for i in range(len(output_im1)):
+					for j in range(len(output_im2)):
+						if annotation1[0]['labels'][i] == annotation2[0]['labels'][j]:
+							IoU = self.IoU(output_im1, output_im2)
+							if IoU>0:
+								if i in box_list:
+									if IoU > matched_boxes[i][0]:
+										matched_boxes[i] = (IoU, j)
+									else:
+										matched_boxes[i] = (IoU, j)
+										box1_list.append(i)
+
+				results.append(matched_boxes)
+
+
+
+		if len(results)==len(self.train_loader):
+			print(results)
+			print("DONE")
+		else:
+			print(len(results), len(self.train_loader))
+
+
+if __name__ == "__main__":
+	geomatch = Test()
+	geomatch.evaluate()
+
+
+
 
 
 
