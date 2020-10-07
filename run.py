22ca857c11036585337f83039d4c3d86f05be639
Ian Leefmans <iel230@nyu.edu>
2020-10-06 20:00:10 -0400

    File to run model with no setup if repository cloned

---
+++ run.py
@@ -0,0 +1,9 @@
+from skinConditionDetect import preprocess as preprocess
+from skinConditionDetect.facealign import FaceAlign, CalculateMatches
+from skinConditionDetect.datahelper2 import CreateDataset, my_collate
+import argparse
+import torchvision
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+print("IMPORTS DONE")
