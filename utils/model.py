eb6765d573827ca7a57bb07d7b9a470345876c38
Ian Leefmans <iel230@nyu.edu>
2020-10-15 17:22:44 -0400

    Finish commenting model.py

--- utils/model.py
+++ utils/model.py
@@ -25,7 +25,8 @@ class STN(nn.Module):
 		super(STN, self).__init__()
 		self.input_size = input_size[-1]
 		self.channels = input_size[1]
-		# Spacial Transformer Localization Network
+
+		# Create Spacial Transformer Localization Network
 		self.kernel_size1 = 7
 		self.kernel_size2 = 5
 		self.localization = nn.Sequential(
@@ -86,20 +87,27 @@ class ConvLayer(nn.Module):
 		"""
 		super(ConvLayer, self).__init__()
 
+		# Channels and kernel size
 		self.in_channels = in_channels
 		self.out_channels =  out_channels
 		self.kernel_size = kernel_size
 
+		# Different types of modules to be used
 		self.conv = nn.Conv2d(self.in_channels, self.out_channels, kernel_size=self.kernel_size)
 		self.pool = nn.MaxPool2d(kernel_size=2, return_indices=True)
 		self.relu = nn.ReLU()
 		self.sigmoid = nn.Sigmoid()
 
+		# Whether or not dropout will be used, also whether or not this block is the final block of a portion 
+		# of the network
 		self.dropout = dropout
 		self.drop = nn.Dropout2d()
 		self.final_layer=final_layer
 
 	def forward(self, x):
+		"""
+			Forward pass of the network
+		"""
 		x = self.conv(x)
 		if self.dropout==True:
 			x = self.drop(x)
@@ -135,22 +143,29 @@ class DeConvLayer(nn.Module):
 		"""
 		super(DeConvLayer, self).__init__()
 
+		# Index and dimensions for unpooling, channels, and kerel size
 		self.pool_index = pool_index
 		self.pre_pool_dims = pre_pool_dims
 		self.in_channels = in_channels
 		self.out_channels = out_channels
 		self.kernel_size = kernel_size
 
+		# Layers used by network
 		self.convT = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=5)
 		self.unpool = nn.MaxUnpool2d(kernel_size=2)
 		self.relu = nn.ReLU()
 		self.sigmoid = nn.Sigmoid()
 
+		# Whether or not dropout will be used, also whether or not this block is the final block of a portion 
+		# of the network
 		self.dropout = dropout
 		self.drop = nn.Dropout2d()
 		self.final_layer = final_layer
 
 	def forward(self, x):
+		"""
+			Forward pass of the network
+		"""
 		x = self.unpool(x, self.pool_index, self.pre_pool_dims)
 		if self.dropout==True:
 			x = self.drop(x)
@@ -168,13 +183,21 @@ class DeConvLayer(nn.Module):
 
 
 class Encoder(nn.Module):
+	"""
+		Encoder portion of each branch of the bi-headed network SCINet2.0
+	"""
 
 	def __init__(self):
 		super(Encoder, self).__init__()
+
+		# Custom convolutional blocks with spacial transformer network integrated between feature mappings
 		self.conv1 = ConvLayer(3, 10, kernel_size=5, dropout=True)
 		self.conv2 = ConvLayer(10, 20, kernel_size=5, dropout=False, final_layer=True)
 		
 	def forward(self, x):
+		"""
+			Forward pass of the network
+		"""
 		stn = STN(x.size())
 		x = stn(x)
 		x, idx1, pre_pool_dim1 = self.conv1(x)
@@ -187,21 +210,32 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
+	"""
+		Decoder portion of each branch of the bi-headed network SCINet2.0
+	"""
 
 	def __init__(self, index_list, dimension_list):
 		"""
 			Args:
 
-					index_list = List of pooling indices returned from Encoder
+					index_list (list): List of pooling indices returned from Encoder
+
+					dimension_list (list): List of prepooling dimensions returned from Encoder
 		"""
 		super(Decoder, self).__init__()
+
+		# index and dimensions for unpooling
 		self.index_list = index_list
 		self.dimension_list = dimension_list
 
+		# Custom de-convolutional blocks with spacial transformer network integrated between feature mappings
 		self.convT1 = DeConvLayer(self.index_list[1], self.dimension_list[1], 20, 10, kernel_size=5, dropout=False)
 		self.convT2 = DeConvLayer(self.index_list[0], self.dimension_list[0], 10, 3, kernel_size=5, dropout=True, final_layer=True)
 
 	def forward(self, x):
+		"""
+			Forward pass of the network
+		"""
 		stn = STN(x.size())
 		x = stn(x)
 		x = self.convT1(x)
@@ -212,21 +246,41 @@ class Decoder(nn.Module):
 		return x
 
 class SCINet20(nn.Module):
+	"""
+		Bi-headed autoencoder with Spacial Transformer Network modules between the convolutional feature mappings
+	"""
 
 	def __init__(self):
-		super(IANet, self).__init__()
+		super(SCINet20, self).__init__()
 
+		# Instantiate encoder for each head of the network
 		self.encoder1 = Encoder()
 		self.encoder2 = Encoder()
 
 	def forward(self, x1, x2):
+		"""
+			Args:
+
+				x1 (torch tensor): tensor to be fed to first autoencoder
+
+				x2 (torch tensor): tensor to be fed to second autoencoder
+
+			Forward pass of the network
+		"""
+
+		# Feed tensors into two heads of the network
 		z1, idx1, dim1 = self.encoder1(x1)
 		z2, idx2, dim2 = self.encoder1(x2)
+
+		# Instantiate decoders using the indexes and dimensions recieved from the 2 encoders
 		decoder1 = Decoder(idx1, dim1)
 		decoder2 = Decoder(idx2, dim2)
 
+		# feed output tensor of encoders into the decoders
 		x1 = decoder1(z1)
 		x2 = decoder2(z2)
+
+		# return output from each autoencoder along with the latent representation of each autoencoder
 		return x1, x2, z1, z2
 
 		
