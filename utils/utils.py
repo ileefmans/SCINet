3647ee0525c7d148fee8009b74bf4ce8c8e3d531
Ian Leefmans <iel230@nyu.edu>
2020-10-16 10:34:09 -0400

    Add class description

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,9 @@
 from preprocess import Image_Process
 
 class ImageProcessTest(Image_Process):
+	"""
+		Class inheriting functionality of Image_Process, to be run in unitest via Travis
+	"""
 	def __init__(self):
 		super(ImageProcessTest, self).__init__()
 		self.test=True
