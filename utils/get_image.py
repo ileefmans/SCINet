e3ad3e77646d9db2b0bfa8d706d44ef543ca9ec6
Ian Leefmans <iel230@nyu.edu>
2020-10-15 21:38:48 -0400

    Remove excess print statements

--- utils/get_image.py
+++ utils/get_image.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 Script to get example image from Bi-Autoencoder
 """
 
-
+# function to process an example sample for inference with SCINet2.0
 def process_for_inference(box, width, height):
     # Create blank image size of picture
     box_im = Image.new('RGB', (width, height))
@@ -41,38 +41,34 @@ def process_for_inference(box, width, height):
 
     return box_im
 
-
-
+# Load model
 model = torch.load("/Users/ianleefmans/Desktop/Insight/Project/Data/model_checkpoints/SCINet_epoch1.pt", map_location=torch.device('cpu'))
+
+# Set model to evaluate
 model.eval()
 
+# Define box coordinates
 box1 = (759, 864, 804, 908)
 box2 = (596,  663,  637,  711)
+
+# Process boxes
 box1_im = process_for_inference(box1, 1440, 1440)
 box2_im = process_for_inference(box2, 1440, 1440)
 box1_im = box1_im.view(1, 3, 256, 256)
 box2_im = box2_im.view(1, 3, 256, 256)
 
-
+# Forward pass of model
 x1, x2, z1, z2 = model(box1_im, box2_im)
 
+# Transform output to PIL Image and print dimensions for evaluating example
 trans = torchvision.transforms.ToPILImage()
 print("\n \n", x1.size(), "\n \n")
 x1 = x1.reshape(3, 256, 256)
 print("\n \n", x1.size(), "\n \n")
 x1 = trans(x1)
+
+# Save image
 x1.save("sampleim.jpg")
-print("\n \n", type(x1), "\n \n")
-
-
-
-
-
-
-
-
-print (x1, x2)
-
 
 
 
