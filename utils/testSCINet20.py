cf40767cad9abbe86b357482e13191ab98370bac
Ian Leefmans <iel230@nyu.edu>
2020-10-15 21:30:31 -0400

    Finish cleaning and commenting testSCINet20.py

--- utils/testSCINet20.py
+++ utils/testSCINet20.py
@@ -18,6 +18,9 @@ import cv2
 
 
 def get_args():
+	"""
+		Function for parsing arguments
+	"""
 	parser = argparse.ArgumentParser(description = "Model Options")
 	parser.add_argument("--model_version", type=int, default=1, help="Version of model to be trained: options = {1:'MVP', ...)")
 	parser.add_argument("--model_path", type=str, default="/Users/ianleefmans/Desktop/Insight/Project/Data/model_checkpoints/SCINet_epoch1.pt", help="Path to saved model")
@@ -48,15 +51,25 @@ class Test:
 
 	def __init__(self):
 
+		# Run function to get argparser
 		self.ops = get_args()
+
+		# Set up path to model
 		self.model_path = self.ops.model_path
+
+		# Set up device, gpu if avalable otherwise cpu
 		self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+		# Load model and send to device
 		self.model = torch.load(self.model_path, map_location=torch.device('cpu'))
 		self.model = self.model.to(self.device)
+
+		# Set up attribute for local or remote testing along with access and secret access keys
 		self.local = self.ops.local
 		self.access_key = self.ops.access_key
 		self.secret_access_key = self.ops.secret_access_key
 
+		# Set up attributes that vary whether or not the script is being run locally or remotely
 		if self.local ==1:
 			self.local=True
 			self.test_pickle_path = self.ops.local_test_pickle_path
@@ -66,6 +79,7 @@ class Test:
 			self.test_pickle_path = self.ops.remote_test_pickle_path
 			self.data_directory = self.ops.remote_data_directory
 
+		# Set up attributes that do not vary
 		self.img_size = self.ops.image_size
 		self.transform = torchvision.transforms.ToTensor()
 		self.batch_size = self.ops.batch_size
@@ -84,10 +98,19 @@ class Test:
 			self.test_loader = DataLoader(dataset=self.testset, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=self.shuffle, collate_fn=my_collate2)
 
 
-	def calculate_metric(self):
-		pass
-
 	def process_for_inference(self, box, width, height):
+		"""
+			Args:
+
+				box (iterable obj): Bounding box coordinates
+
+				width: (int): Width of image
+
+				height: (int): Height of image
+
+			Method to create blank image with bouding box filled in
+		"""
+
 		# Create blank image size of picture
 		box_im = Image.new('RGB', (width, height))
 		box_im = cv2.cvtColor(np.array(box_im), cv2.COLOR_RGB2BGR)
@@ -99,11 +122,9 @@ class Test:
 		y2 = int(box[3])
 
 		# Draw bounding boxes
-
 		cv2.rectangle(box_im,(x1,y1),(x2,y2),(0,255,0),-1) 
 
 		# Transform to PIL Image for preprocessing before entering SCINet
-		#box_im = cv2.cvtColor(box_im, cv2.COLOR_BGR2RBG)
 		box_im = Image.fromarray(box_im)
 		box_im = box_im.convert('RGB')
 
@@ -118,20 +139,27 @@ class Test:
 
 
 	def IoU(self, input1, input2):
+		"""
+			Args:
+				input1(torch tensor): First tensor of image to be evaluated via Intersection over Union
+
+				input2(torch tensor): Second tensor of image to be evaluated via Intersection over Union
+
+			Method to evaluate intersection over union
+		"""
 		union = np.count_nonzero(np.array(input1) + np.array(input2))
 		total  = np.count_nonzero(np.array(input1)) + np.count_nonzero(np.array(input2))
 		intersection = total-union
 		IoU = intersection/union
-
-		print(union, intersection)
-
 		return IoU
 
 
-
-
 	def evaluate(self):
+		"""
+			Method to evaluate SCINet2.0's performance
+		"""
 
+		# If remote send everything to GPU
 		if self.local==False:
 			torch.set_default_tensor_type(torch.cuda.FloatTensor)
 			print("\n \n EVERYTHING TO CUDA \n \n")
@@ -141,10 +169,14 @@ class Test:
 			start_epoch, loss = self.load_checkpoint(self.model, self.optimizer, self.model_name)
 			print("\n \n [WEIGHTS LOADED]")
 
+		# Set model to evaluate
 		with torch.no_grad():
 			self.model.eval()
 
+			# Initialize list for results
 			results = []
+
+			# Iterate over test set
 			for image1, image2, annotation1, annotation2, landmark1, landmark2 in tqdm(self.test_loader, desc= "Test "):
 
 				# Get dimensions of both images
@@ -206,39 +238,22 @@ class Test:
 				for i in range(len(output_im1)):
 					for j in range(len(output_im2)):
 						if annotation1[0]['labels'][i] == annotation2[0]['labels'][j]:
-
-
-
-							#IoU = self.IoU(output_im1[i], output_im2[j])
-							CE = nn.functional.binary_cross_entropy(output_im1[i], output_im2[j], reduction='sum')
-							print(CE)
-							if i in box_list:
-								print(matched_boxes[i][0])
-								if CE < matched_boxes[i][0]:
-									matched_boxes[i] = (CE, j)
-								else:
-									matched_boxes[i] = (CE, j)
-									box1_list.append(i)
-
-
-
-							# if IoU>0:
-							# 	if i in box_list:
-							# 		if IoU > matched_boxes[i][0]:
-							# 			matched_boxes[i] = (IoU, j)
-							# 		else:
-							# 			matched_boxes[i] = (IoU, j)
-							# 			box1_list.append(i)
-
+							IoU = self.IoU(output_im1[i], output_im2[j])
+							if IoU>0:
+								if i in box_list:
+									if IoU > matched_boxes[i][0]:
+										matched_boxes[i] = (IoU, j)
+									else:
+										matched_boxes[i] = (IoU, j)
+										box1_list.append(i)
 				results.append(matched_boxes)
-
-
 
 		if len(results)==len(self.test_loader):
 			print(results)
 			print("DONE")
 		else:
 			print(len(results), len(self.test_loader))
+			print results
 
 
 if __name__ == "__main__":
